d4c0150800ea652f45e4643343fb71104418266b
paragonMattL <matt@paragontechdev.com>
2017-03-25 15:03:01 -0700

    name change

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-var url = 'mongodb://localhost:27017/wordApp';
+var url = 'mongodb://localhost:27017/wordApp2';
 mongoose.connect(url);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -22,7 +22,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'wordApp');
+app.set('view engine', 'wordApp2');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
